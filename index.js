794f6d017a18676ff2514bebe5ca2e6945df17c6
Peace <peaceegede32@gmail.com>
2023-08-08 01:36:45 +0100

    Assignment2

---
+++ index.js
@@ -0,0 +1,10 @@
+// question 1
+var myfirstnum = "my name is michelle"
+var mysecnum = "I am a beautiful girl"
+console.log("my firstnum " + " " + "my secnum")
+var firstnum = 100
+var secnum = 20
+let result1 = firstnum + secnum
+let result2 = firstnum * secnum
+console.log("firstnum + secnum = " + " " + result1)
+console.log("firstnum * secnum ="  + " " + result2)
